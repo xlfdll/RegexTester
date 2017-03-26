d895c6a346e57c671d6b4ecf7c6d8f75b4063d2a
xlfdll <xlfdll@gmail.com>
2017-03-25 19:56:21 -0700

    Version 4.0: Removed TaskHelper

--- RegexTester/Commands/RegexCommands.cs
+++ RegexTester/Commands/RegexCommands.cs
@@ -13,178 +13,206 @@ using RegexTester.Windows;
 
 namespace RegexTester.Commands
 {
-    public static class RegexCommands
-    {
-        static RegexCommands()
-        {
-            RegexCommands.MatchCommand = new RelayCommand<Object>
-            (
-                delegate
-                {
-                    TaskWindow taskWindow = new TaskWindow(TaskHelper.Match, null)
-                    {
-                        Owner = ApplicationHelper.MainWindow
-                    };
+	public static class RegexCommands
+	{
+		static RegexCommands()
+		{
+			RegexCommands.MatchCommand = new RelayCommand<Object>
+			(
+				delegate
+				{
+					TaskWindow taskWindow = new TaskWindow
+					(
+						delegate (Object argument)
+						{
+							return RegexHelper.Match(RegexState.Current.Input);
+						},
+						null
+					)
+					{
+						Owner = ApplicationHelper.MainWindow
+					};
 
-                    taskWindow.ShowDialog();
+					taskWindow.ShowDialog();
 
-                    if (taskWindow.Exception == null)
-                    {
-                        RegexResult regexResult = taskWindow.Result as RegexResult;
+					if (taskWindow.Exception == null)
+					{
+						RegexResult regexResult = taskWindow.Result as RegexResult;
 
-                        if (regexResult != null)
-                        {
-                            RegexState.Current.Result = regexResult;
+						if (regexResult != null)
+						{
+							RegexState.Current.Result = regexResult;
 
-                            ApplicationHelper.Settings.History.AddRecentRegexPattern(RegexState.Current.Input.RegexPattern);
-                        }
-                        else
-                        {
-                            ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An unknown error occurred in the process of Match operation.", new ApplicationException("TaskWindow.Result is null"));
-                        }
-                    }
-                    else
-                    {
-                        ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An error occurred in the process of Match operation.", taskWindow.Exception.InnerException);
-                    }
-                },
-                delegate { return RegexCommands.CanRegexCommandExecute; }
-            );
+							ApplicationHelper.Settings.History.AddRecentRegexPattern(RegexState.Current.Input.RegexPattern);
+						}
+						else
+						{
+							ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An unknown error occurred in the process of Match operation.", new ApplicationException("TaskWindow.Result is null"));
+						}
+					}
+					else
+					{
+						ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An error occurred in the process of Match operation.", taskWindow.Exception.InnerException);
+					}
+				},
+				delegate { return RegexCommands.CanRegexCommandExecute; }
+			);
 
-            RegexCommands.SplitCommand = new RelayCommand<Object>
-            (
-                delegate
-                {
-                    TaskWindow taskWindow = new TaskWindow(TaskHelper.Split, null)
-                    {
-                        Owner = ApplicationHelper.MainWindow
-                    };
+			RegexCommands.SplitCommand = new RelayCommand<Object>
+			(
+				delegate
+				{
+					TaskWindow taskWindow = new TaskWindow
+					(
+						delegate (Object argument)
+						{
+							return RegexHelper.Split(RegexState.Current.Input);
+						},
+						null
+					)
+					{
+						Owner = ApplicationHelper.MainWindow
+					};
 
-                    taskWindow.ShowDialog();
+					taskWindow.ShowDialog();
 
-                    if (taskWindow.Exception == null)
-                    {
-                        String[] results = taskWindow.Result as String[];
+					if (taskWindow.Exception == null)
+					{
+						String[] results = taskWindow.Result as String[];
 
-                        if (results != null)
-                        {
-                            StringBuilder sb = new StringBuilder();
+						if (results != null)
+						{
+							StringBuilder sb = new StringBuilder();
 
-                            foreach (String item in results)
-                            {
-                                sb.AppendLine(item);
-                            }
+							foreach (String item in results)
+							{
+								sb.AppendLine(item);
+							}
 
-                            RegexState.Current.Input.Text = sb.ToString();
+							RegexState.Current.Input.Text = sb.ToString();
 
-                            ApplicationHelper.Settings.History.AddRecentRegexPattern(RegexState.Current.Input.RegexPattern);
-                        }
-                        else
-                        {
-                            ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An unknown error occurred in the process of Split operation.", new ApplicationException("TaskWindow.Result is null"));
-                        }
-                    }
-                    else
-                    {
-                        ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An error occurred in the process of Split operation.", taskWindow.Exception.InnerException);
-                    }
-                },
-                delegate { return RegexCommands.CanRegexCommandExecute; }
-            );
+							ApplicationHelper.Settings.History.AddRecentRegexPattern(RegexState.Current.Input.RegexPattern);
+						}
+						else
+						{
+							ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An unknown error occurred in the process of Split operation.", new ApplicationException("TaskWindow.Result is null"));
+						}
+					}
+					else
+					{
+						ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An error occurred in the process of Split operation.", taskWindow.Exception.InnerException);
+					}
+				},
+				delegate { return RegexCommands.CanRegexCommandExecute; }
+			);
 
-            RegexCommands.ReplaceCommand = new RelayCommand<Object>
-            (
-                delegate
-                {
-                    TaskWindow taskWindow = new TaskWindow(TaskHelper.Replace, null)
-                    {
-                        Owner = ApplicationHelper.MainWindow
-                    };
+			RegexCommands.ReplaceCommand = new RelayCommand<Object>
+			(
+				delegate
+				{
+					TaskWindow taskWindow = new TaskWindow
+					(
+						delegate (Object argument)
+						{
+							return RegexHelper.Replace(RegexState.Current.Input);
+						},
+						null
+					)
+					{
+						Owner = ApplicationHelper.MainWindow
+					};
 
-                    taskWindow.ShowDialog();
+					taskWindow.ShowDialog();
 
-                    if (taskWindow.Exception == null)
-                    {
-                        String result = taskWindow.Result as String;
+					if (taskWindow.Exception == null)
+					{
+						String result = taskWindow.Result as String;
 
-                        if (result != null)
-                        {
-                            RegexState.Current.Input.Text = result;
+						if (result != null)
+						{
+							RegexState.Current.Input.Text = result;
 
-                            ApplicationHelper.Settings.History.AddRecentRegexPattern(RegexState.Current.Input.RegexPattern);
-                            ApplicationHelper.Settings.History.AddRecentReplacePattern(RegexState.Current.Input.ReplacePattern);
-                        }
-                        else
-                        {
-                            ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An unknown error occurred in the process of Replace operation.", new ApplicationException("TaskWindow.Result is null"));
-                        }
-                    }
-                    else
-                    {
-                        ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An error occurred in the process of Replace operation.", taskWindow.Exception.InnerException);
-                    }
-                },
-                delegate { return RegexCommands.CanRegexCommandExecute; }
-            );
+							ApplicationHelper.Settings.History.AddRecentRegexPattern(RegexState.Current.Input.RegexPattern);
+							ApplicationHelper.Settings.History.AddRecentReplacePattern(RegexState.Current.Input.ReplacePattern);
+						}
+						else
+						{
+							ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An unknown error occurred in the process of Replace operation.", new ApplicationException("TaskWindow.Result is null"));
+						}
+					}
+					else
+					{
+						ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An error occurred in the process of Replace operation.", taskWindow.Exception.InnerException);
+					}
+				},
+				delegate { return RegexCommands.CanRegexCommandExecute; }
+			);
 
-            RegexCommands.MeasureCommand = new RelayCommand<Object>
-            (
-                delegate
-                {
-                    TaskWindow taskWindow = new TaskWindow(TaskHelper.Measure, null)
-                    {
-                        Owner = ApplicationHelper.MainWindow
-                    };
+			RegexCommands.MeasureCommand = new RelayCommand<Object>
+			(
+				delegate
+				{
+					TaskWindow taskWindow = new TaskWindow
+					(
+						delegate (Object argument)
+						{
+							return RegexHelper.Measure(RegexState.Current.Input);
+						},
+						null
+					)
+					{
+						Owner = ApplicationHelper.MainWindow
+					};
 
-                    taskWindow.ShowDialog();
+					taskWindow.ShowDialog();
 
-                    if (taskWindow.Exception == null)
-                    {
-                        // Defensive code, since Int64 variable cannot be null
+					if (taskWindow.Exception == null)
+					{
+						// Defensive code, since Int64 variable cannot be null
 
-                        try
-                        {
-                            DialogHelper.ShowMeasureResultMessageBox(Convert.ToInt64(taskWindow.Result));
-                        }
-                        catch (FormatException formatEx)
-                        {
-                            ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An unknown error occurred in the process of Measure operation.", formatEx);
-                        }
-                    }
-                    else
-                    {
-                        ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An error occurred in the process of Measure operation.", taskWindow.Exception.InnerException);
-                    }
-                },
-                delegate { return RegexCommands.CanRegexCommandExecute; }
-            );
+						try
+						{
+							DialogHelper.ShowMeasureResultMessageBox(Convert.ToInt64(taskWindow.Result));
+						}
+						catch (FormatException formatEx)
+						{
+							ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An unknown error occurred in the process of Measure operation.", formatEx);
+						}
+					}
+					else
+					{
+						ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "An error occurred in the process of Measure operation.", taskWindow.Exception.InnerException);
+					}
+				},
+				delegate { return RegexCommands.CanRegexCommandExecute; }
+			);
 
-            RegexCommands.EditCommand = new RelayCommand<String>
-            (
-                (String parameter) =>
-                {
-                    EditWindow editWindow = new EditWindow()
-                    {
-                        Owner = ApplicationHelper.MainWindow
-                    };
+			RegexCommands.EditCommand = new RelayCommand<String>
+			(
+				(String parameter) =>
+				{
+					EditWindow editWindow = new EditWindow()
+					{
+						Owner = ApplicationHelper.MainWindow
+					};
 
-                    editWindow.PatternTextBox.SetBinding(TextBox.TextProperty,
-                        new Binding(parameter) { UpdateSourceTrigger = UpdateSourceTrigger.Explicit });
+					editWindow.PatternTextBox.SetBinding(TextBox.TextProperty,
+						new Binding(parameter) { UpdateSourceTrigger = UpdateSourceTrigger.Explicit });
 
-                    editWindow.ShowDialog();
-                }
-            );
-        }
+					editWindow.ShowDialog();
+				}
+			);
+		}
 
-        public static RelayCommand<Object> MatchCommand { get; }
-        public static RelayCommand<Object> SplitCommand { get; }
-        public static RelayCommand<Object> ReplaceCommand { get; }
-        public static RelayCommand<Object> MeasureCommand { get; }
-        public static RelayCommand<String> EditCommand { get; }
+		public static RelayCommand<Object> MatchCommand { get; }
+		public static RelayCommand<Object> SplitCommand { get; }
+		public static RelayCommand<Object> ReplaceCommand { get; }
+		public static RelayCommand<Object> MeasureCommand { get; }
+		public static RelayCommand<String> EditCommand { get; }
 
-        private static Boolean CanRegexCommandExecute
-        {
-            get { return RegexState.Current != null && !String.IsNullOrEmpty(RegexState.Current.Input.RegexPattern); }
-        }
-    }
+		private static Boolean CanRegexCommandExecute
+		{
+			get { return RegexState.Current != null && !String.IsNullOrEmpty(RegexState.Current.Input.RegexPattern); }
+		}
+	}
 }
--- RegexTester/Helpers/JobHelper.cs
+++ RegexTester/Helpers/JobHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 using Xlfdll.Windows.Presentation.Dialogs;
 
@@ -7,74 +9,98 @@ using RegexTester.Windows;
 
 namespace RegexTester.Helpers
 {
-    public static class JobHelper
-    {
-        public static void LoadRegexJob(String path)
-        {
-            TaskWindow taskWindow = new TaskWindow(TaskHelper.LoadJob, path)
-            {
-                Owner = ApplicationHelper.MainWindow
-            };
+	public static class JobHelper
+	{
+		public static void LoadRegexJob(String path)
+		{
+			TaskWindow taskWindow = new TaskWindow
+			(
+				delegate (Object argument)
+				{
+					return RegexHelper.Match(RegexState.Current.Input);
+				},
+				path
+			)
+			{
+				Owner = ApplicationHelper.MainWindow
+			};
 
-            taskWindow.ShowDialog();
+			taskWindow.ShowDialog();
 
-            if (taskWindow.Exception == null)
-            {
-                RegexInput regexInput = taskWindow.Result as RegexInput;
+			if (taskWindow.Exception == null)
+			{
+				RegexInput regexInput = taskWindow.Result as RegexInput;
 
-                if (regexInput != null)
-                {
-                    RegexState.Current.Input = regexInput;
-                    RegexState.Current.Update(path);
-                }
-            }
-            else
-            {
-                ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "Invalid job file format.", taskWindow.Exception.InnerException);
-            }
-        }
+				if (regexInput != null)
+				{
+					RegexState.Current.Input = regexInput;
+					RegexState.Current.Update(path);
+				}
+			}
+			else
+			{
+				ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "Invalid job file format.", taskWindow.Exception.InnerException);
+			}
+		}
 
-        public static void SaveRegexJob(String path)
-        {
-            TaskWindow taskWindow = new TaskWindow(TaskHelper.SaveJob, path)
-            {
-                Owner = ApplicationHelper.MainWindow
-            };
+		public static void SaveRegexJob(String path)
+		{
+			TaskWindow taskWindow = new TaskWindow
+			(
+				delegate (Object argument)
+				{
+					FileHelper.SaveRegexInput(argument.ToString(), RegexState.Current.Input);
 
-            taskWindow.ShowDialog();
+					return null;
+				},
+				path
+			)
+			{
+				Owner = ApplicationHelper.MainWindow
+			};
 
-            if (taskWindow.Exception == null)
-            {
-                RegexState.Current.Update(path);
-            }
-            else
-            {
-                ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "Unexpected exception happened.", taskWindow.Exception.InnerException);
-            }
-        }
+			taskWindow.ShowDialog();
 
-        public static void LoadTextDocument(String path)
-        {
-            TaskWindow taskWindow = new TaskWindow(TaskHelper.LoadText, path)
-            {
-                Owner = ApplicationHelper.MainWindow
-            };
+			if (taskWindow.Exception == null)
+			{
+				RegexState.Current.Update(path);
+			}
+			else
+			{
+				ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "Unexpected exception happened.", taskWindow.Exception.InnerException);
+			}
+		}
 
-            taskWindow.ShowDialog();
+		public static void LoadTextDocument(String path)
+		{
+			TaskWindow taskWindow = new TaskWindow
+			(
+				delegate (Object argument)
+				{
+					// Use Encoding.Default to eliminate incorrect characters 
+					return File.ReadAllText(argument.ToString(), Encoding.Default);
+				},
+				path
+			)
+			{
+				Owner = ApplicationHelper.MainWindow
+			};
 
-            if (taskWindow.Exception == null)
-            {
-                String text = taskWindow.Result as String;
+			taskWindow.ShowDialog();
 
-                if (text != null)
-                {
-                    RegexState.Current.Input.Text = text;
-                }
-            }
-            else
-            {
-                ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "Invalid job file format.", taskWindow.Exception.InnerException);
-            }
-        }
-    }
+			if (taskWindow.Exception == null)
+			{
+				String text = taskWindow.Result as String;
+
+				if (text != null)
+				{
+					RegexState.Current.Input.Text = text;
+				}
+			}
+			else
+			{
+				ExceptionMessageBox.Show(ApplicationHelper.MainWindow, ApplicationHelper.Metadata.AssemblyTitle, "Invalid job file format.", taskWindow.Exception.InnerException);
+			}
+		}
+	}
 }
