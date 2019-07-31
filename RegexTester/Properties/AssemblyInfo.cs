8b31a35c6c9ffb2cbb6c1d93bf5bf705b4593e31
xlfdll <xlfdll@gmail.com>
2019-07-30 19:11:49 -0700

    Added Git build number generation script to use deterministic version representation

--- RegexTester/Properties/AssemblyInfo.cs
+++ RegexTester/Properties/AssemblyInfo.cs
@@ -2,6 +2,8 @@
 using System.Runtime.InteropServices;
 using System.Windows;
 
+using Xlfdll;
+
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
@@ -49,6 +51,6 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.*")]
+[assembly: AssemblyVersion("4.1." + BuildInfo.Build)]
 
 [assembly: GuidAttribute("b644ca18-51d4-4d15-8c3c-81f42e66a98e")]
